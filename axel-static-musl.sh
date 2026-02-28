3d14c4b61ed1f61501b1d92f2ea0db51327da164
Matt Rinsch <gfunkmonk@mailbox.org>
2026-02-28 04:04:36 -0500

    customize-o-mite

--- axel-static-musl.sh
+++ axel-static-musl.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+ORANGE="\033[38;2;255;165;0m"
+LEMON="\033[38;2;255;244;79m"
+TAWNY="\033[38;2;204;78;0m"
+HELIOTROPE="\033[38;2;223;115;255m"
+VIOLET="\033[38;2;143;0;255m"
+MINT="\033[38;2;152;255;152m"
+AQUA="\033[38;2;18;254;202m"
+TOMATO="\033[38;2;255;99;71m"
+NC="\033[0m"
+
 ARCH=${ARCH:-x86_64}
 
 ##map arch to Alpine minirootfs URL and QEMU binary name
@@ -33,29 +43,29 @@ esac
 
 TARBALL="${ALPINE_URL##*/}"
 
-##install some dependencies
+echo -e "${AQUA}= install some dependencies${NC}"
 sudo apt -y install wget curl binutils
 
-##download alpine rootfs
+echo -e "${HELIOTROPE}= download alpine rootfs${NC}"
 wget -c "${ALPINE_URL}"
 
-##extract rootfs
+echo -e "${MINT}= extract rootfs${NC}"
 mkdir pasta
 tar xf "${TARBALL}" -C pasta/
 
-##copy resolv.conf into the folder
+echo -e "${TOMATO}= copy resolv.conf into the folder${NC}"
 cp /etc/resolv.conf ./pasta/etc/
 
-##setup QEMU for cross-arch builds
+echo -e "${TAWNY}= setup QEMU for cross-arch builds${NC}"
 if [ -n "${QEMU_ARCH}" ]; then
   sudo apt -y install qemu-user-static
   sudo mkdir -p ./pasta/usr/bin/
   sudo cp "/usr/bin/qemu-${QEMU_ARCH}-static" "./pasta/usr/bin/"
 fi
 
-##if fails in cat command add inside chroot line this command "cat src/css_.c >> src/css.c"
+echo -e "${ORANGE}= if fails in cat command add inside chroot line this command 'cat src/css_.c >> src/css.c'${NC}"
 
-##mount, bind and chroot into dir
+echo -e "${VIOLET}= mount, bind and chroot into dir${NC}"
 sudo mount -t proc none ./pasta/proc/
 sudo mount --rbind /dev ./pasta/dev/
 sudo mount --rbind /sys ./pasta/sys/
@@ -80,10 +90,11 @@ upx \
 perl && curl -L -O 'https://github.com/axel-download-accelerator/axel/releases/download/v2.17.14/axel-2.17.14.tar.gz' && \
 tar xf axel-2.17.14.tar.gz && \
 cd axel-2.17.14/ && \
-./configure CC=gcc LDFLAGS='-static' CFLAGS='-O3' && \
+./configure CC=gcc LDFLAGS='-static' CFLAGS='-O3 -Wno-unterminated-string-initialization' && \
 make -j\$(nproc) && \
 strip axel && \
 upx --ultra-brute axel"
 mkdir -p dist
 cp "./pasta/axel-2.17.14/axel" "dist/axel-${ARCH}"
 tar -C dist -cJf "dist/axel-${ARCH}.tar.xz" "axel-${ARCH}"
+echo -e "${LEMON}= All done!${NC}"
